6694a422dbbc621062742383fd8dc5a370c4458d
Abdullah Alrasheed <a@maman.ae>
2025-07-23 17:41:34 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), icon()],
-  base: '',
+  // base: '/',
 
   i18n: {
       routing: {
